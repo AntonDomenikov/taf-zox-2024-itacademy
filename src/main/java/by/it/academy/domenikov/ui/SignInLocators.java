95cce9951f9047a550ee5fbf891cf9965015ff7c
AntonDomenikov <a.domenikov@gmail.com>
2024-04-27 12:54:37 +0300

    Added locators classes

---
+++ src/main/java/by/it/academy/domenikov/ui/SignInLocators.java
@@ -0,0 +1,10 @@
+package by.it.academy.domenikov.ui;
+
+public class SignInLocators {
+    final static String TEST_LOGIN_EMAIL1 = "elizabetkolins@rover.info";
+    final static String TEST_LOGIN_PASSWORD1 = "qwerty12345";
+    final static String LOGIN_EMAIL_FIELD_CSS_SELECTOR = "#email";
+    final static String LOGIN_PASSWORD_FIELD_XPATH = "//input[@id='password']";
+    final static String LOGIN_BUTTON_XPATH = "//button[text()='Sign In']";
+    final static String SEARCH_FIELD_XPATH = "/html/body/div[1]/div[2]/div/header[1]/div[2]/div[1]/div/div/form/input[3]";
+}
---
+++ src/main/java/by/it/academy/domenikov/ui/SignUpLocators.java
@@ -0,0 +1,11 @@
+package by.it.academy.domenikov.ui;
+
+public class SignUpLocators {
+    final static String SIGNUP_BUTTON_XPATH = "//*[@id='headerContent']/div/header[1]/div[2]/div[1]/div/a";
+    final static String CREATE_NEW_ACCOUNT_LINK_XPATH ="//a[@href='/account/register']";
+    final static String REGISTER_EMAIL_FIELD_CSS_SELECTOR ="#email";
+    final static String REGISTER_PASSWORD_FIELD_CSS_SELECTOR = "#password";
+    final static String CREATE_ACCOUNT_BUTTON_XPATH = "//*[@id='mainContent']/div[1]/div/form/div[3]/button";
+    final static String ACCOUNT_DETAILS_TEXT_XPATH ="//*[@id='mainContent']/div[1]/div[1]/p";
+
+}
