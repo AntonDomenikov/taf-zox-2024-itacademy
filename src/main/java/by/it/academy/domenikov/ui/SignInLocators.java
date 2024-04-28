f69a4a1049216b843e98eec197b27a9331f0a8f3
AntonDomenikov <a.domenikov@gmail.com>
2024-04-28 18:59:28 +0300

    Added

--- src/main/java/by/it/academy/domenikov/ui/SignInLocators.java
+++ src/main/java/by/it/academy/domenikov/ui/SignInLocators.java
@@ -6,5 +6,4 @@ public class SignInLocators {
     final static String LOGIN_EMAIL_FIELD_CSS_SELECTOR = "#email";
     final static String LOGIN_PASSWORD_FIELD_XPATH = "//input[@id='password']";
     final static String LOGIN_BUTTON_XPATH = "//button[text()='Sign In']";
-    final static String SEARCH_FIELD_XPATH = "/html/body/div[1]/div[2]/div/header[1]/div[2]/div[1]/div/div/form/input[3]";
 }
--- src/main/java/by/it/academy/domenikov/ui/SiteLocators.java
+++ src/main/java/by/it/academy/domenikov/ui/SiteLocators.java
@@ -1,7 +1,5 @@
 package by.it.academy.domenikov.ui;
 
 public class SiteLocators {
-    final static String SEARCH_FIELD_SUBMIT_BTN_XPATH = "//form[@data-gtm-form-interact-id='1']//button[@id='search-button']";
-    final static String BUTTERFLY_KISSES_LINK_XPATH = "//div[@class='h-full relative group']/a[@href='/products/butterfly-kisses']";
     final static String SIGNOUT_BTN_XPATH = "//button[text()='Sign out']";
 }
