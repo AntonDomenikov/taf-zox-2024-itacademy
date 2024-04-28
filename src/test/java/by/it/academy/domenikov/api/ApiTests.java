e70e2e9e46507bdeb18a644b4f21c36b112ba26e
AntonDomenikov <a.domenikov@gmail.com>
2024-04-28 15:33:46 +0300

    Finished writing code for 2 TestMethods

--- src/test/java/by/it/academy/domenikov/api/ApiTests.java
+++ src/test/java/by/it/academy/domenikov/api/ApiTests.java
@@ -2,31 +2,14 @@ package by.it.academy.domenikov.api;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.net.CacheRequest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ApiTests {
 
-    @Test
-    public void testLogin1() {
-        given()
-                .contentType(ContentType.JSON)
-                .body("{\"login\": \"moksemnovikov@gmail.com\"}")
-                .when()
-                .post("api.litres.ru/foundation/api/auth/login-available")
-                .then()
-                .statusCode(200)
-                .body("status", equalTo(200));
-    }
-
-    @Test
-    public void testLogin2() {
-        given().header("content-type", "application/json").body("{\"login\":\"test@test.ru\"}").when().
-                post("api.litres.ru/foundation/api/auth/login-available").then().
-                statusCode(200).body("status", equalTo(200)).and().
-                body("payload.data.available", equalTo(false)).
-                body("error", equalTo(null));
-    }
 }
