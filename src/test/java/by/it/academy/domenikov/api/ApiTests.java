e72576e3a7edce096026fe6a6faa96ee53a395da
AntonDomenikov <a.domenikov@gmail.com>
2024-04-28 19:00:37 +0300

    Cleared not used import files

--- src/test/java/by/it/academy/domenikov/api/ApiTests.java
+++ src/test/java/by/it/academy/domenikov/api/ApiTests.java
@@ -1,14 +1,8 @@
 package by.it.academy.domenikov.api;
 
-import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.net.CacheRequest;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 public class ApiTests {
     @Test
