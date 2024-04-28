272a52b3eea0b82a7208b90ec426cb18c0890def
AntonDomenikov <a.domenikov@gmail.com>
2024-04-28 17:29:20 +0300

    Added

--- src/test/java/by/it/academy/domenikov/api/ApiTests.java
+++ src/test/java/by/it/academy/domenikov/api/ApiTests.java
@@ -13,10 +13,11 @@ import static org.hamcrest.Matchers.equalTo;
 public class ApiTests {
     @Test
     @DisplayName("Api 1: login with non-existent account")
-    public void testLogin2() {
+    public void testLogin1() {
         given().
-                body("").
                 contentType("multipart/form-data").
+                multiPart("email", "testetst@yte.com").
+                multiPart("password","yuyqwhjhsd").
                 when().
                 post("https://zox.la/account/login?_data=routes%2F%28%24locale%29.account.login").
                 then().
